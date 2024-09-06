9e1045398e824484df8569af017595796f9a5bfc
ristedev <47864975+ristedev@users.noreply.github.com>
2024-09-06 00:43:29 -0400

    feat: add eleventy-navigation plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,24 @@
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
+  const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
-  // eleventyConfig.addPassthroughCopy({'static':'.'});
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
+  eleventyConfig.addPassthroughCopy("assets");
+
+  // Log all collections after the build process
+  // eleventyConfig.on("afterBuild", function (collections) {
+  //   console.log("Collections:", collections.post);
+  // });
 
   return {
-    templateFormats: ['liquid', 'md', 'html'],
+    templateFormats: ["liquid", "md", "html"],
     dir: {
-      input: 'src',
-      output: '_site',
+      input: "src",
+      output: "_site",
       // the following are relative to `input` above
-      includes: '_includes',
-      layouts: '_layouts',
-      data: '_data'
-    }
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data",
+    },
   };
 };
