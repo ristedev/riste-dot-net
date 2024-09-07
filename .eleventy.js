9c05a4f429cfd989f7dcdb795503ad03d85d4c28
ristedev <47864975+ristedev@users.noreply.github.com>
2024-09-07 00:24:02 -0400

    refactor: move plugin require statment out of module.exports function

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
-module.exports = function (eleventyConfig) {
-  const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
+module.exports = function (eleventyConfig) {
+  
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   eleventyConfig.addPassthroughCopy("assets");
