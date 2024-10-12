d5b6f766b223bc70e3465845c03d0ca7a7564a3f
ristedev <47864975+ristedev@users.noreply.github.com>
2024-10-12 01:31:38 -0400

    feat: fetch and display the latest commit timestamp & link

---
+++ assets/js/fetchLatestCommit.js
@@ -0,0 +1,20 @@
+async function fetchLastCommit() {
+  const response = await fetch('https://api.github.com/repos/ristedev/blog/commits/main');
+  if (!response.ok) {
+      document.getElementById('last-updated').innerText = 'Error fetching commit data.';
+      return;
+  }
+  const commit = await response.json();
+  const commitDate = new Date(commit.commit.committer.date); // Get the commit date
+  const commitHash = commit.sha; // Get the commit hash
+
+  // Format the date
+  const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', hour12: true, timeZone: 'UTC' };
+  const formattedDate = commitDate.toLocaleString('en-US', options); // Format in the specified format
+
+  // Update the inner HTML of the paragraph with the latest commit date and link
+  document.getElementById('last-updated').innerHTML = `Last built on <a href="https://github.com/ristedev/blog/commit/${commitHash}" target="_blank">${formattedDate} UTC</a>`;
+}
+
+// Call the function to fetch the latest commit
+document.addEventListener("DOMContentLoaded", fetchLastCommit);
