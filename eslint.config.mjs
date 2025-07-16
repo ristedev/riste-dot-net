56a261f4a1a5e7c73fc07de9b1a65a37ad1e95e4
ristedev <47864975+ristedev@users.noreply.github.com>
2025-07-16 12:30:57 -0400

    chore: add eslint & prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js"
+import globals from "globals"
+import { defineConfig } from "eslint/config"
+import prettierPlugin from "eslint-plugin-prettier"
+import prettierConfig from "eslint-config-prettier"
+
+export default defineConfig([
+  {
+    ignores: [
+      "node_modules/",
+      "_site/",
+      "src/pagefind/"
+    ],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    plugins: { js, prettier: prettierPlugin },
+    extends: ["js/recommended", prettierConfig],
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  {
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
+  },
+  {
+    files: ["**/*.{js,mjs,cjs}"],
+    languageOptions: { globals: globals.browser },
+  },
+  {
+    files: ["src/pagefind/**/*.js"],
+    languageOptions: { sourceType: "module" },
+  },
+])
